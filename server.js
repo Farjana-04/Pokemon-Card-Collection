fb5d408adfa3c53c5f688630707537741878022a
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-10 10:51:45 -0500

    Create an Express application and set the port, and Configure Express with various middleware

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
